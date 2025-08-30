2254ba086a0450cc3a1701f92ff4c7a295c041f4
gajendraLoma <141911581+gajendraLoma@users.noreply.github.com>
2025-08-30 11:31:54 +0700

    remove console

--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -10,7 +10,6 @@ export default async function BlogPage({data}: {data: any}) {
   const perPage = 16;
   const initialData: PostByCatResponse = await fetchPostByCat('category', '', 'post', perPage, 1);
   const t = await getTranslations();
-  console.log("initialData",data)
   return (
     <main className="min-h-screen bg-gray-50">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-8">
